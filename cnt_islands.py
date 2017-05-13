186f36c8c64fd5c28a0392dc08e3187058c6e02f
Fialkovskiy <Dmitriy.Fialkovskiy@nielsen.com>
2017-05-13 22:04:30 +0300

    insert recursion function

--- cnt_islands.py
+++ cnt_islands.py
@@ -4,44 +4,55 @@ Created on Sat May 13 20:13:12 2017
 
 @author: fialdm01
 """
-neib=set()
-def chk(islands, i,j):
-    try:
-        if islands[i][j] == 0:
-            return None
-        else:
-            neib.add((i,j))
-    except IndexError:
-        return None
-    
-    return chk(islands, i+1,j), chk(islands, i,j+1)
-
-chk(tst11,0,0)
-
 
 def cnt_islands(islands):
     isl_count = 0
-    neib_cnt = set()
+    neib_g = set()
+    neib_l = set()
+    
+    def chk(islands, i,j):
+        try:
+            if islands[i][j] == 0:
+                return None
+            else:
+                neib_l.add((i,j))
+        except IndexError:
+            return None
+        
+        return chk(islands, i+1,j), chk(islands, i,j+1) #, chk(islands, i-1,j), chk(islands, i,j-1)
+    
+    cnt_zero = 1    
     for i in range(len(islands)):
         for j in range(len(islands[i])):
-            if islands[i][j] == 1:                
-                try:
-                    if islands[i+1][j] == 1:
-                        neib_cnt.add((i+1, j))
-                        neib_cnt.add((i, j))
-                    if islands[i][j+1] == 1:
-                        neib_cnt.add((i, j+1))
-                        neib_cnt.add((i, j))
-                except IndexError:
-                     pass
-                                    
-    if isl_count == 0 and neib_cnt != set():
+            if islands[i][j] == 1 and (i,j) not in neib_g:            
+                                
+                chk(islands, i,j)
+                for x in neib_l:
+                    if x in neib_g:
+                        cnt_zero = 0
+                        print(x)
+                        break
+                neib_g.update(neib_l)
+                if cnt_zero == 1:
+                    isl_count += 1
+            cnt_zero = 1
+    if isl_count == 0 and neib != set():
         isl_count = 1
     return isl_count
 
+chk(tst1, 0,0)
 
-
-
+#                while 1:
+#                    try:
+#                        if islands[i+1][j] == 1:
+#                            neib_cnt.add((i+1, j))
+#                            neib_cnt.add((i, j))
+#                        if islands[i][j+1] == 1:
+#                            neib_cnt.add((i, j+1))
+#                            neib_cnt.add((i, j))
+#                        i,j +=1
+#                    except IndexError:
+#                        pass
 
 cnt_islands(tst1)
 cnt_islands(tst2)
